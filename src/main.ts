a0e55a1063e72be395ada704841d51ddede3cc31
rarepepi <pepimartinez@protonmail.com>
2022-09-26 18:01:20 -0400

    msg

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.port, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
 bootstrap();
